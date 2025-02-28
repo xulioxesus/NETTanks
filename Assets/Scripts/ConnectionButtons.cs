307d7ab4f665bf8402c5267fc6016cf988ecb7a5
xulioxesus <julio.leon@firescalestudios.com>
2025-02-28 08:58:21 +0100

    2.4 Network Authority

---
+++ Assets/Scripts/ConnectionButtons.cs
@@ -0,0 +1,14 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class ConnectionButtons : MonoBehaviour
+{
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+### 2.4 Network Authority
+
+Se añade botón de conexión como Host
+
+Se cambia la autoridad sobre los NetworkTransform del Prefab Player para que sea del Owner.
