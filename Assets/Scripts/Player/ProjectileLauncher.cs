e9f258328ee880058f505b8afb09150bf97a8d6e
Julio Jesús León Pérez <xulioxesus@gmail.com>
2025-03-04 23:53:34 +0100

    2.9 Firing Improvements

--- Assets/Scripts/Player/ProjectileLauncher.cs
+++ Assets/Scripts/Player/ProjectileLauncher.cs
@@ -9,10 +9,19 @@ public class ProjectileLauncher : NetworkBehaviour
     [SerializeField] private Transform projectileSpawnPoint;
     [SerializeField] private GameObject serverProjectilePrefab;
     [SerializeField] private GameObject clientProjectilePrefab;
+    [SerializeField] private GameObject muzzleFlash;
+    [SerializeField] private Collider2D playerCollider;
+    
 
     [Header("Settings")]
     [SerializeField] private float projectileSpeed;
+    [SerializeField] private float fireRate;
+    [SerializeField] private float muzzleFlashDuration;
+
     private bool shouldFire;
+    private float previousFireTime;
+    private float muzzleFlashTimer;
+
 
     public override void OnNetworkSpawn()
     {
@@ -30,29 +39,66 @@ public class ProjectileLauncher : NetworkBehaviour
     private void HandlePrimaryFire(bool shouldFire)
     {
         this.shouldFire = shouldFire;
+
     }
 
     void Update()
     {
+        if (muzzleFlashTimer > 0f)
+        {
+            muzzleFlashTimer -= Time.deltaTime;
+
+            if (muzzleFlashTimer <= 0f)
+            {
+                muzzleFlash.SetActive(false);
+            }
+        }
+
         if(!IsOwner) {return; }
         if(!shouldFire) {return; }
 
+        if (Time.time < (1 / fireRate) + previousFireTime) { return; }
+
         PrimaryFireServerRpc(projectileSpawnPoint.position, projectileSpawnPoint.up);
         SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+
+        previousFireTime = Time.time;
+
     }
 
     private void SpawnDummyProjectile(Vector3 position, Vector3 up)
     {
+        muzzleFlash.SetActive(true);
+        muzzleFlashTimer = muzzleFlashDuration;
+
         GameObject projectileInstance = Instantiate(clientProjectilePrefab, position, Quaternion.identity);
         projectileInstance.transform.up = up;
+
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up * projectileSpeed;
+        }
+
     }
 
     [Rpc(SendTo.Server)]
     private void PrimaryFireServerRpc(Vector3 position, Vector3 up)
     {
-        GameObject projectileInstance = Instantiate(serverProjectilePrefab, position, Quaternion.identity);
-        projectileInstance.transform.up = up;
-        SpawnDummyProjectileClientRpc(position, up); 
+        GameObject projectileInstance = Instantiate(
+            serverProjectilePrefab,
+            position,
+            Quaternion.identity);
+
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up * projectileSpeed;
+        }
+
+        SpawnDummyProjectileClientRpc(position, up);
     }
 
     [Rpc(SendTo.ClientsAndHost)]
--- README.md
+++ README.md
@@ -21,8 +21,10 @@ Crear `PlayerAiming.cs` para seguir el cursor del ratón con la torreta.
 ### 2.7 Networked Projectiles
 
 Crear el Prefab ProjectileBase con dos variantes:
-    - ProjectileClient (Con un sprite 2D para la bala)
-    - ProjectileServer
+
+- ProjectileClient (Con un sprite 2D para la bala)
+
+- ProjectileServer
 
 En el ProjectileBase hay dos scripts `LifeTime.cs` y `DestroySelfOnContact`
 
@@ -35,3 +37,17 @@ Revisar matriz de colisiones para Física y para Física2D.
 Se crea un script `ProjectileLauncher` para disparar: crea balas que solo ven los clientes a nivel local e invocan la creación de las balas en el servidor.
 
 Este script se asocia al prefab Player.
+
+### 2.9 Firing Improvements
+
+Se modifica el prefab Player:
+
+- añadir un componente con un sprite para mostrar la explosión en el cañón (muzzleFlash).
+
+Modificar el código de ProjectileLauncher para:
+
+- Añadir retardo entre disparos
+
+- Evitar que la bala colisione con el propio tanque.
+
+- Proporcionar la velocidad de la bala y que se mueva utilizando el Rigidbody2D de la misma.
